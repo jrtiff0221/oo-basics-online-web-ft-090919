002a025fc8594835cf78174e0d617a3c1e331175
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-18 19:01:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -23,30 +23,4 @@ class Book
     @page_count
   end
  
-   
-class Book
-  def initialize(title)
-    @title = title
-  end
- 
-  def title
-    @title
-  end
- 
-  def author=(author)
-    @author = author
-  end
- 
-  def author
-    @author
-  end
- 
-  def page_count=(num)
-    @page_count = num
-  end
- 
-  def page_count
-    @page_count
-  end
-end
- 
+end   
