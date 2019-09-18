9817c0498e1ba2f740888d85f67e948412a8490f
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-18 18:57:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -19,4 +19,20 @@ class Book
     @page_count = num
   end
  
+  def page_count
+    @page_count
+  end
+ 
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
 end
